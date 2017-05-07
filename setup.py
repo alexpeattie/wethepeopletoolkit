569f6398bc66819229a04b2b53cbcf2ad09aa3c9
Alex Peattie <alexpeattie@gmail.com>
2017-05-07 17:14:28 +0100

    Don't pull in README.md on PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 from setuptools import setup
 import sys
-import os
-
-def read(fname):
-  return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 try:
   from PyQt4.QtCore import QT_VERSION_STR
@@ -12,14 +8,14 @@ except ImportError:
 
 setup(
   name = "wethepeopletoolkit",
-  version = "1.0",
+  version = "1.1",
   author = "Alex Peattie",
   author_email = "me@alexpeattie.com",
   description = ("A project for analyzing and visualizing data from the Obama-era 'We the People' petitions site."),
   license = "MIT",
   keywords = "wethepeople petitions datascience analysis",
   url = "https://github.com/alexpeattie/wethepeopletoolkit",
-  download_url = 'https://github.com/alexpeattie/wethepeopletoolkit/archive/1.0.tar.gz',
+  download_url = 'https://github.com/alexpeattie/wethepeopletoolkit/archive/1.1.tar.gz',
   install_requires=[
     'bs4',
     'click',
@@ -36,6 +32,5 @@ setup(
   entry_points='''
     [console_scripts]
     wethepeopletoolkit=main:cli
-  ''',
-  long_description=read('README.md'),
+  '''
 )
